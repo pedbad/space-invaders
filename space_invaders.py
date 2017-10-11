43d913bd6bdd33d943da85947fb1cd0e86a2c592
pedbad <pb357@cam.ac.uk>
2017-10-11 10:06:02 +0100

    collision detection added

--- space_invaders.py
+++ space_invaders.py
@@ -3,6 +3,7 @@
 
 import turtle
 import os
+import math
 
 #Set up the screen
 
@@ -95,6 +96,14 @@ def fire_bullet():
     bulletstate = "fire"
 
 
+def isCollision(t1,t2):
+  distance = math.sqrt(math.pow(t1.xcor() - t2.xcor(), 2) + math.pow(t1.ycor() - t2.ycor(),2))
+  if distance < 15:
+    return True
+  else:
+    return False
+
+
 
 #create keyboard bindings
 turtle.listen()
@@ -133,6 +142,15 @@ while True:
   if bullet.ycor() > 275:
     bullet.hideturtle()
     bulletstate = "ready"
+
+  #Check for collision between bullet and enemy
+  if isCollision(bullet, enemy):
+    #Reset the bullet
+    bullet.hideturtle()
+    bulletstate = "ready"
+    bullet.setposition(0, -400)
+    #Reset the enemy
+    enemy.setposition(-200, 250)
 
 
 
