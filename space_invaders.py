ab0a82cd736dce8b805551ca02963adb03b3f7d7
pedbad <pb357@cam.ac.uk>
2017-10-11 11:55:11 +0100

    respawn enemies at a random location

--- space_invaders.py
+++ space_invaders.py
@@ -155,7 +155,9 @@ while True:
       bulletstate = "ready"
       bullet.setposition(0, -400)
       #Reset the enemy
-      enemy.setposition(-200, 250)
+      x = random.randint(-200, 200)
+      y = random.randint(100, 200)
+      enemy.setposition(x, y)
 
     #Check for collision between enemy and player
     if isCollision(player, enemy):
